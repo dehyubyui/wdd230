338dcbb42fd8c04758535a75779e267e406187bb
wdd130cassell <cas22015@byui.edu>
2023-02-24 14:08:23 +0000

    addad a form page

---
+++ Chamber/styles/lesson8/banner.js
@@ -0,0 +1,4 @@
+let thedate = new Date();
+if (thedate.getDay() == 5) {
+   document.querySelector('#banner').getElementsByClassName.display = 'flex';
+}
---
+++ Chamber/styles/lesson8/date.js
@@ -0,0 +1,10 @@
+let dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]
+let d =new Date();
+let dayName = dayNames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fullDate = dayName + ", " + d.getDate() + " " + monthName + " "  + d.getFullYear();
+document.getElementById("currentDate").textContent = fullDate;
+
+const copyrightyear = document.getElementById("copyrightyear");
+copyrightyear.textContent = new Date().getFullYear();
